d41cc4a73f30bda5dbf53b170f6d658d89bf3723
Wesley Felix <wesley.felixdelima96@hotmail.com>
2021-01-26 21:37:47 -0300

    só o user id = 1 pode criar e editar usuários

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,9 +60,11 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('empresa.listar') }}">Empresas</a>
                             </li>
+                            @if( Auth::user()->id == 1)
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('usuario.listar') }}">Usuarios</a>
                             </li>
+                            @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
